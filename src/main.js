bd9699fb6b01fb71c822e528440478b219c66b97
rasedul99 <rasedul2199@gmail.com>
2022-09-24 14:28:29 +0600

    navbar

---
+++ src/main.js
@@ -0,0 +1,14 @@
+let openBtn = document.querySelector(".bi-list");
+let closeBtn = document.querySelector(".bi-x");
+let menu = document.querySelector(".navbar__menu");
+
+openBtn.addEventListener("click", () => {
+  openBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("hide");
+  menu.classList.toggle("hidden");
+});
+closeBtn.addEventListener("click", () => {
+  openBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("hide");
+  menu.classList.toggle("hidden");
+});
